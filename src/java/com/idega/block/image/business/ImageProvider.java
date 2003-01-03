2f3b0271fae0d750ad97723e22f30bba4457fb6a
Thomas Hilbig <thomas@idega.com>
2003-01-03 19:12:06 +0000

    *** empty log message ***

--- src/java/com/idega/block/image/business/ImageProvider.java
+++ src/java/com/idega/block/image/business/ImageProvider.java
@@ -4,6 +4,8 @@ import javax.ejb.*;
 
 public interface ImageProvider extends com.idega.business.IBOService
 {
- public java.util.ArrayList getImagesFromTo(com.idega.core.data.ICFile p0,int p1,int p2) throws java.rmi.RemoteException, java.sql.SQLException;
  public int getImageCount(com.idega.core.data.ICFile p0) throws java.rmi.RemoteException;
+ public java.util.ArrayList getImagesFromTo(com.idega.core.data.ICFile p0,int p1,int p2)throws java.sql.SQLException, java.rmi.RemoteException;
+ public int uploadImage(java.io.InputStream p0,java.lang.String p1, java.lang.String p2,int width, int height, com.idega.block.image.data.ImageEntity p3)throws javax.ejb.CreateException, java.rmi.RemoteException;
+ public void setHeightAndWidthOfOriginalImageToEntity(int width, int height, com.idega.block.image.data.ImageEntity imageEntity) throws javax.ejb.TransactionRolledbackLocalException;
 }
--- src/java/com/idega/block/image/business/ImageProviderBean.java
+++ src/java/com/idega/block/image/business/ImageProviderBean.java
@@ -1,6 +1,7 @@
 package com.idega.block.image.business;
 
 import java.io.BufferedInputStream;
+import java.io.FileInputStream;
 import java.io.InputStream;
 import java.util.Arrays;
 import java.sql.SQLException;
@@ -8,28 +9,37 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Iterator;
 
+import javax.ejb.CreateException;
+import javax.ejb.TransactionRolledbackLocalException;
 import javax.media.jai.PlanarImage;
 import javax.media.jai.RegistryMode;
 import javax.media.jai.OperationRegistry;
 import javax.media.jai.*;
+import javax.transaction.SystemException;
+import javax.transaction.TransactionRolledbackException;
+import javax.transaction.UserTransaction;
 
 
 import com.idega.block.image.data.ImageEntity;
+import com.idega.block.image.data.ImageEntityHome;
 import com.idega.block.image.presentation.AdvancedImage;
 import com.idega.business.IBOServiceBean;
 import com.idega.core.data.ICFile;
 import com.idega.presentation.Image;
 import com.sun.media.jai.codec.MemoryCacheSeekableStream;
-/*
 /**
- * @author Thomas
- *
- * To change this generated comment edit the template variable "typecomment":
- * Window>Preferences>Java>Templates.
- * To enable and disable the creation of type comments go to
- * Window>Preferences>Java>Code Generation.
+ *  * 
+ * 
+ * Title:         idegaWeb
+ * Description:   This class is mainly used by the ImageGallery class to 
+ *                get, to store and to change stored images.
+ *                  
+ * Copyright:     Copyright (c) 2003
+ * Company:       idega software
+ * @author <a href="mailto:thomas@idega.is">Thomas Hilbig</a>
+ * @version 1.0
  */
-public class ImageProviderBean extends IBOServiceBean implements ImageProvider{
+public class ImageProviderBean extends IBOServiceBean implements ImageProvider {
 
   
   public ImageProviderBean(){}
@@ -62,7 +72,80 @@ public class ImageProviderBean extends IBOServiceBean implements ImageProvider{
     }
     return result;  
   }
+  
+  public int uploadImage(InputStream inputStream, String mimeType, String name, int width, int height, ImageEntity parent) 
+    throws CreateException  {
+    
+    UserTransaction trans=null;
+    int id = -1;
+    try
+    {
+      trans = this.getSessionContext().getUserTransaction();
+      trans.begin();
+      // create new image entity
+      ImageEntityHome imageEntityHome = (ImageEntityHome)com.idega.data.IDOLookup.getHomeLegacy(ImageEntity.class);
+      ImageEntity imageEntity;
+      // create throws CreateException
+      imageEntity = imageEntityHome.create();
+      // store value of File
+      imageEntity.setFileValue(inputStream);
+      imageEntity.setMimeType(mimeType);
+      imageEntity.setName(name);
+      imageEntity.setWidth(Integer.toString(width));
+      imageEntity.setHeight(Integer.toString(height));
+      // store throws RuntimeException
+      imageEntity.store();
+      // set new entity as a child 
+      // addChild throws SQLException
+      parent.addChild(imageEntity);
+      id = imageEntity.getID();
+      trans.commit();
+    }
+    catch (Exception e) {
+      System.err.println(e.getMessage());
+//      e.printStackTrace();
+      if(trans!=null) {
+        try {
+          trans.rollback();
+        }
+        catch (SystemException se) {
+          se.printStackTrace();
+        } 
+      }
+      throw new CreateException("There was an error storing the modified image. Message was: "+e.getMessage());
+    }
+    return id;
+  } 
+  
+  
+  public void setHeightAndWidthOfOriginalImageToEntity(int width, int height, ImageEntity imageEntity) 
+    throws TransactionRolledbackLocalException {
+    UserTransaction trans = null;    
+    try {
+      trans = this.getSessionContext().getUserTransaction();
+      trans.begin();  
+      imageEntity.setWidth(Integer.toString(width));
+      imageEntity.setHeight(Integer.toString(height));
+      imageEntity.store();
+      trans.commit();
+    }
+    catch (Exception e) {
+      System.err.println(e.getMessage());
+//      e.printStackTrace();
+      if(trans!=null) {
+        try {
+          trans.rollback();
+        }
+        catch (SystemException se) {
+          se.printStackTrace();
+        } 
+      }
+      throw new TransactionRolledbackLocalException("There was an error storing height and width of the image. Message was: "+ e.getMessage());
+    }
+  } 
+    
 }
+  
   
  
 
