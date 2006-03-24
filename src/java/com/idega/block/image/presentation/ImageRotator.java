b9065ba92f24303181c4b76a0b0947c8ad8ddbd9
Grimur Jonsson <gimmi@idega.com>
2006-03-24 18:12:31 +0000

    uses the new cache system

--- src/java/com/idega/block/image/presentation/ImageRotator.java
+++ src/java/com/idega/block/image/presentation/ImageRotator.java
@@ -13,97 +13,94 @@ import java.io.IOException;
 import java.rmi.RemoteException;
 import java.util.ArrayList;
 import java.util.List;
+
 import javax.faces.context.FacesContext;
-import org.apache.commons.httpclient.HttpException;
+
 import org.apache.webdav.lib.WebdavResource;
-import org.apache.webdav.lib.WebdavResources;
+
 import com.idega.business.IBOLookup;
 import com.idega.business.IBORuntimeException;
 import com.idega.presentation.IWContext;
 import com.idega.presentation.Image;
 import com.idega.presentation.PresentationObjectTransitional;
-import com.idega.slide.business.IWSlideSession;
-import com.idega.slide.util.WebdavExtendedResource;
-
+import com.idega.slide.business.IWSlideService;
 
 public class ImageRotator extends PresentationObjectTransitional {
 
 	private static final String ATTRIBUTE_ROTATOR = "image_rotator_";
-	
+
 	private String iFolderURI;
+
 	private String iWidth;
+
 	private String iHeight;
+
 	private String iAlt;
-	
-	/* (non-Javadoc)
+
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see com.idega.presentation.PresentationObjectTransitional#encodeBegin(javax.faces.context.FacesContext)
 	 */
 	public void encodeBegin(FacesContext context) throws IOException {
 		super.encodeBegin(context);
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see com.idega.presentation.PresentationObjectTransitional#encodeChildren(javax.faces.context.FacesContext)
 	 */
 	public void encodeChildren(FacesContext context) throws IOException {
 		super.encodeChildren(context);
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see com.idega.presentation.PresentationObjectTransitional#encodeEnd(javax.faces.context.FacesContext)
 	 */
 	public void encodeEnd(FacesContext arg0) throws IOException {
 		super.encodeEnd(arg0);
 	}
-	
-	/* (non-Javadoc)
+
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see com.idega.presentation.PresentationObjectTransitional#initializeComponent(javax.faces.context.FacesContext)
 	 */
 	protected void initializeComponent(FacesContext context) {
 		IWContext iwc = IWContext.getIWContext(context);
 
-		List imageURLs = (List) iwc.getApplicationAttribute(ATTRIBUTE_ROTATOR + getICObjectID() + iFolderURI);
-		
-		if (imageURLs == null) {
-			try {
-				IWSlideSession session = (IWSlideSession) IBOLookup.getSessionInstance(iwc, IWSlideSession.class);
-				WebdavExtendedResource resource = session.getWebdavResource(getFolderURI());
-				if (resource.isCollection()) {
-					WebdavResources children = resource.getChildResources();
-					WebdavResource[] resources = children.listResources();
-					imageURLs = getImageURLs(resources);
-					iwc.setApplicationAttribute(ATTRIBUTE_ROTATOR + getICObjectID() + iFolderURI, imageURLs);
-				}
-			}
-			catch (HttpException e) {
-				e.printStackTrace();
-			}
-			catch (RemoteException e) {
-				throw new IBORuntimeException(e);
-			}
-			catch (IOException e) {
-				e.printStackTrace();
-			}
+		List imagePaths = null;
+		try {
+			IWSlideService service = (IWSlideService) IBOLookup
+					.getServiceInstance(getIWApplicationContext(),
+							IWSlideService.class);
+			ArrayList result = new ArrayList();
+			imagePaths = service
+					.getChildPathsExcludingFoldersAndHiddenFiles(getFolderURI());
+		} catch (RemoteException e) {
+			throw new IBORuntimeException(e);
 		}
-		
-		if (imageURLs != null) {
-			String imageURL = getRandomURL(imageURLs);
-			
-	    Image image = new Image(imageURL);
-	    if (getAlt() != null) {
-	    		image.setAlt(getAlt());
-	    }
-	    if (getHeight() != null) {
-	    		image.setHeight(getHeight());
-	    }
-	    if (getWidth() != null) {
-	    		image.setWidth(getWidth());
-	    }
-	    
-	    getChildren().add(image);
+
+		if (imagePaths != null) {
+			String imageURL = getRandomURL(imagePaths);
+
+			Image image = new Image(imageURL);
+			if (getAlt() != null) {
+				image.setAlt(getAlt());
+			}
+			if (getHeight() != null) {
+				image.setHeight(getHeight());
+			}
+			if (getWidth() != null) {
+				image.setWidth(getWidth());
+			}
+			getChildren().add(image);
 		}
 	}
-	
+
 	private List getImageURLs(WebdavResource[] resources) {
 		List list = new ArrayList();
 		for (int i = 0; i < resources.length; i++) {
@@ -112,15 +109,15 @@ public class ImageRotator extends PresentationObjectTransitional {
 				list.add(resource.getPath());
 			}
 		}
-		
+
 		return list;
 	}
-	
+
 	private String getRandomURL(List imageURLs) {
-    int num = (int) (Math.random() * imageURLs.size());
-    return (String) imageURLs.get(num);
+		int num = (int) (Math.random() * imageURLs.size());
+		return (String) imageURLs.get(num);
 	}
-	
+
 	/**
 	 * @see javax.faces.component.UIComponentBase#saveState(javax.faces.context.FacesContext)
 	 */
@@ -146,11 +143,11 @@ public class ImageRotator extends PresentationObjectTransitional {
 		setHeight((String) values[3]);
 		setWidth((String) values[4]);
 	}
-	
+
 	public String getAlt() {
 		return iAlt;
 	}
-	
+
 	public void setAlt(String alt) {
 		iAlt = alt;
 	}
@@ -158,23 +155,23 @@ public class ImageRotator extends PresentationObjectTransitional {
 	public String getFolderURI() {
 		return iFolderURI;
 	}
-	
+
 	public void setFolderURI(String folderURI) {
 		iFolderURI = folderURI;
 	}
-	
+
 	public String getHeight() {
 		return iHeight;
 	}
-	
+
 	public void setHeight(String height) {
 		iHeight = height;
 	}
-	
+
 	public String getWidth() {
 		return iWidth;
 	}
-	
+
 	public void setWidth(String width) {
 		iWidth = width;
 	}
