b03cb1971eb9aeeba09e93785e237c8cf0d7eee1
Valdas Žemaitis <valdas@idega.com>
2013-01-04 17:09:16 +0200

    Removed useless stuff

--- src/java/com/idega/block/image/IWBundleStarter.java
+++ src/java/com/idega/block/image/IWBundleStarter.java
@@ -4,34 +4,21 @@ import com.idega.idegaweb.IWBundle;
 import com.idega.idegaweb.IWBundleStartable;
 
 /**
- * 
- * 
+ *
+ *
  *  Last modified: $Date: 2008/10/20 11:54:30 $ by $Author: laddi $
- * 
+ *
  * @author <a href="mailto:eiki@idega.com">eiki</a>
  * @version $Revision: 1.3 $
  */
 public class IWBundleStarter implements IWBundleStartable {
 
+	@Override
 	public void start(IWBundle starterBundle) {
-		//todo register auto image resizer
-//		   try {
-//	            IWSlideService service = (IWSlideService) IBOLookup.getServiceInstance(iwac,IWSlideService.class);
-//	           
-//	            
-//	            //add it as a slide change listener for caching purposes
-//	            service.addIWSlideChangeListeners(service);
-//	            
-//	        } catch (IBOLookupException e) {
-//	            e.printStackTrace();
-//	        } catch (RemoteException e) {
-//	            e.printStackTrace();
-//	        }
-//		
-//		
-//		
+		//TODO register auto image resizer
 	}
-	
+
+	@Override
 	public void stop(IWBundle starterBundle) {
 		// nothing to do
 	}
